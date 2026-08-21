37a31e93f9a0ff354883e300d09e474d7501c208
delishasrao <delishasrao@gmail.com>
2026-08-21 12:36:07 +0530

    Add files via upload

---
+++ prog1_day12.c
@@ -0,0 +1,53 @@
+//Q23: Write a program to calculate library fine based on late days as follows: 
+/*First 5 days late: ₹2/day 
+Next 5 days late: ₹4/day 
+Next 20 days days late: ₹6/day 
+More than 30 days: Membership Cancelled.
+
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+Fine ₹8
+
+Input 2:
+8
+Output 2:
+Fine ₹22
+
+Input 3:
+15
+Output 3:
+Fine ₹60
+
+Input 4:
+31
+Output 4:
+Membership Cancelled
+
+*/
+#include <stdio.h>
+int main()
+{
+	int dayslate,fine;
+	printf("enter number of days late:");
+	scanf("%d", &dayslate);
+	if (dayslate==0) {
+	printf("Fine Rs.0",fine);
+    }
+	else if (dayslate<=5) {
+	fine=2*dayslate;
+	printf("Fine Rs.%d", fine);
+	}
+	else if (dayslate<=10) {
+	fine=4*dayslate;
+	printf("Fine Rs.%d", fine);
+	}
+	else if (dayslate<=30) {
+	fine=6*dayslate;
+	printf("Fine Rs.%d", fine);
+	}
+	else if (dayslate>30)
+	printf("Membership Cancelled");
+return 0;
+}	
